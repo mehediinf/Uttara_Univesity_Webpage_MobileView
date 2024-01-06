a59842bead9cefd0b02ebe112704204bf64c6235
mehedi-hasan <mehediinform@gmail.com>
2024-01-07 03:48:14 +0600

    1st Commit Uttara_Univesity_Webpage_MobileView

--- app/src/main/java/com/example/uttara_univesity_webpage_mobileview/MainActivity.java
+++ app/src/main/java/com/example/uttara_univesity_webpage_mobileview/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.uttara_univesity_webpage_mobileview;
 
+import static android.provider.Contacts.Settings.getSetting;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        webView = findViewById(R.id.webView_Id);
+
+
+        //Java code/File Enable .
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //Another page view by one application .
+        webView.setWebViewClient(new WebViewClient());
+
+
+        webView.loadUrl("https://uttarauniversity.edu.bd/");
+
+
     }
 }
